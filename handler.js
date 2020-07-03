9b28ddbf4223cb8509bd8fddf9ab989d3cb1f509
gareth <gareth@mccumskey.com>
2020-07-03 14:11:56 +0200

    Initial

---
+++ handler.js
@@ -0,0 +1,22 @@
+'use strict';
+const fs = require('fs')
+
+module.exports.hello = async event => {
+  const dircontents = fs.readdirSync('/opt')
+  console.log(dircontents)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
